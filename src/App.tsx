75eba28b3eb3d6bbcff2bd9768aa6ad595eb1d79
Stepan Susorov <steven.susorov@gmail.com>
2023-11-24 17:05:07 -0700

    Cleaned up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React, { useState } from "react";
 
 export default function App(): JSX.Element {
-  const [query, setQuery] = useState(""); // New state to store the query
-
-  const runSpider = async () => {
-    try {
-      const res = await fetch(
-        `http://127.0.0.1:8000/myapp/api/scrape/?query=${encodeURIComponent(
-          query
-        )}`
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleQueryChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setQuery(event.target.value);
-  };
 
   return (
-    <div>
+    <div className="">
       
     </div>
   );
