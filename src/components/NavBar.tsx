7d2b0dd82ff5ac055620bb76f9737f9a26ac03f4
Stepan Susorov <steven.susorov@gmail.com>
2023-11-24 18:04:03 -0700

    Completed Navigation Bar structure

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,19 +6,21 @@ export default function NavBar(props: any): JSX.Element {
         <div className={`flex transition-transform ${status} flex-col shadow-xl w-48 min-h-screen`}>
             <div className="flex justify-between items-center p-3">
                 <h1 className="text-purple-80">Project Name</h1>
-                <button onClick={() => status === "" ? setStatus("") : setStatus("")}>button</button>
+                <button onClick={() => status === "" ? setStatus("") : setStatus("")}>
+                    <img className="w-6 transition opacity-50 hover:opacity-100" src="/public/dashboard.png"/>
+                </button>
             </div>
             <div className="flex flex-col flex-grow space-y-4 p-3 justify-center">
                 <h1>Score:</h1>
-                <ul>
+                <ul className="flex flex-col gap-2">
                     {/* <li><input type="text" placeholder={props.players[0]}/></li>
                     <li><input type="text" placeholder={props.players[1]}/></li>
                     <li><input type="text" placeholder={props.players[2]}/></li>
                     <li><input type="text" placeholder={props.players[3]}/></li> */}
-                    <li><input type="text" placeholder="Player 1"/></li>
-                    <li><input type="text" placeholder="Player 2"/></li>
-                    <li><input type="text" placeholder="Player 3"/></li>
-                    <li><input type="text" placeholder="Player 4"/></li>
+                    <li className="flex justify-between"><input className="w-1/2" type="text" placeholder="Player 1"/><h1 className="pr-6">0</h1></li>
+                    <li className="flex justify-between"><input className="w-1/2" type="text" placeholder="Player 2"/><h1 className="pr-6">0</h1></li>
+                    <li className="flex justify-between"><input className="w-1/2" type="text" placeholder="Player 3"/><h1 className="pr-6">0</h1></li>
+                    <li className="flex justify-between"><input className="w-1/2" type="text" placeholder="Player 4"/><h1 className="pr-6">0</h1></li>
                 </ul>
             </div>
             
