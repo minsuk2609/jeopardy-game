b6ce14d1de5b1dc36e6d915e914abf10834e88d9
Ba6ySHark <steven.susorov@gmail.com>
2023-11-25 21:50:35 -0700

    Added values to the board

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,7 @@
 export default function Cell(props: any): JSX.Element {
     return (
-        <div className="w-20 h-20 flex flex-col items-center justify-center rounded-lg border-solid border-2">
-            <h1>{props.value}</h1>
+        <div className="w-20 h-20 flex flex-col items-center justify-center rounded-lg border-solid border-2 hover:bg-gray-200">
+            <h1 className="text-purple-80">{props.value}</h1>
         </div>
     );
 }
--- src/components/CellColumn.tsx
+++ src/components/CellColumn.tsx
@@ -3,11 +3,11 @@ import Cell from "./Cell";
 export default function CellColumn(props: any): JSX.Element {
     return (
         <div className="flex flex-col gap-2">
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell value={100}/>
+            <Cell value={200}/>
+            <Cell value={300}/>
+            <Cell value={400}/>
+            <Cell value={500}/>
         </div>
     );
 }
