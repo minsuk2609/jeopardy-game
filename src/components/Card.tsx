1d8dcecf4fedebbebca64a87ecb8bf3d6bde7027
Ba6ySHark <steven.susorov@gmail.com>
2023-11-26 15:17:23 -0700

    Added a close button to the card and an icon for it

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,9 @@
 export default function Card(props: any): JSX.Element {
     return (props.trigger) ? (
         <div className="absolute top-0 left-0 z-50 w-full h-full rounded-lg bg-slate-200">
-            
+            <button className="absolute right-0 top-0 p-3" onClick={() => props.setTrigger(false)}>
+                <img className="w-6 h-6 opacity-50 hover:opacity-100" src="/public/close.png"/>
+            </button>
         </div>
     ) : (
         <div className="hidden"></div>
