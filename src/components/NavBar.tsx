f1114f2dfc389f6c90d6cf37d6be64938c43e1bb
Ba6ySHark <steven.susorov@gmail.com>
2023-11-26 20:51:22 -0700

    Added positioning to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,20 @@
+import "/styles.css";
+
 export default function NavBar({players, setPlayer, status, setStatus}: any): JSX.Element {
     return (
         <div>
             <button className={`absolute top-3 left-3 z-50 ${status ? "" : "hidden"}`} onClick={() => {
                 setStatus(!status);
             }}>
-                <img className="w-6 transition opacity-50 hover:opacity-100" src="public/dashboard.png"/>
+                <img className="w-6 transition opacity-50 hover:opacity-100" src={"dashboard.png"}/>
             </button>
-            <div className={`flex transition ${status ? "hidden" : ""} flex-col flex-grow shadow-xl w-48 min-h-screen`}>
+            <div className={`flex ${status ? "md:hidden nav--class--mode-1" : "md:flex nav--class--mode-2"} flex-col flex-grow shadow-xl w-48 min-h-screen bg-[#fff]`}>
                 <div className="flex justify-between items-center p-3">
                     <h1 className="text-purple-80">Project Name</h1>
                     <button onClick={() => {
                         setStatus(!status);
                     }}>
-                        <img className="w-6 transition opacity-50 hover:opacity-100" src="public/dashboard.png"/>
+                        <img className="w-6 transition opacity-50 hover:opacity-100" src="dashboard.png"/>
                     </button>
                 </div>
                 <div className="flex flex-col flex-grow space-y-4 p-3 justify-center">
