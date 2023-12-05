74611af37e7d0d5fb54aec8144c4e393b31f3c9a
Stepan Susorov <steven.susorov@gmail.com>
2023-12-04 18:41:42 -0700

    Added questions file

---
+++ src/util/questions.ts
@@ -0,0 +1,27 @@
+export default {
+    "topic_1": {
+        "name": "Encryption",
+        "questions": {
+            "100": {
+                "text": "The cipher based on moving each letter of plaintext a fixed number of letters down the alphabet is called:",
+                "answer": {
+                    "1": "Caesar cipher",
+                    "2": "Vigenere cipher",
+                    "3": "RSA",
+                    "4": "Pigpen cipher"
+                },
+                "correct": ""
+            },
+            "200": {
+                "text": "",
+                "answer": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            }
+        }
+    }
+}
