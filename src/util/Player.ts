8b924ce91ff3c099409c0db800cfb39647549a84
Stepan Susorov <steven.susorov@gmail.com>
2023-12-06 14:56:57 -0700

    Updated Player interface

--- src/util/Player.ts
+++ src/util/Player.ts
@@ -1,9 +1,11 @@
 export default class Player {
     name: string;
     score: number;
+    active: boolean;
 
     constructor(some_name: string) {
         this.name = some_name;
         this.score = 0;
+        this.active = false;
     }
 }
