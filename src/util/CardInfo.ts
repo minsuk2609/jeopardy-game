5111c6abb4a5349cf309ac6fc38f30465b039be2
Stepan Susorov <steven.susorov@gmail.com>
2023-12-06 14:27:49 -0700

    Updated cardInfo interface

--- src/util/CardInfo.ts
+++ src/util/CardInfo.ts
@@ -2,10 +2,12 @@ export default class CardInfo {
     category: string;
     question: string;
     answer: string;
+    answerList: any;
 
-    constructor(category: string, question: string, answer: string) {
+    constructor(category: string, question: string, answer: string, answerList: any) {
         this.category = category;
         this.question = question;
         this.answer = answer;
+        this.answerList = answerList;
     }
 }
