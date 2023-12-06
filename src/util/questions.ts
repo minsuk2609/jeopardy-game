3a844f47b4e8681003ba08f1fffeab0c7c4ed82e
Stepan Susorov <steven.susorov@gmail.com>
2023-12-06 14:28:12 -0700

    Updated questions

--- src/util/questions.ts
+++ src/util/questions.ts
@@ -1,20 +1,270 @@
-export default {
-    "topic_1": {
+const data: any =  {
+    "1": {
         "name": "Encryption",
         "questions": {
             "100": {
                 "text": "The cipher based on moving each letter of plaintext a fixed number of letters down the alphabet is called:",
-                "answer": {
+                "answers": {
                     "1": "Caesar cipher",
                     "2": "Vigenere cipher",
                     "3": "RSA",
                     "4": "Pigpen cipher"
                 },
+                "correct": "Ceasar cipher"
+            },
+            "200": {
+                "text": "An algorithm used to encrypt a non fixed size of data to a fixed size of jumbled characters is called:",
+                "answers": {
+                    "1": "Adding",
+                    "2": "Condensing",
+                    "3": "Hashing",
+                    "4": "Modulus"
+                },
+                "correct": "Hashing"
+            },
+            "300": {
+                "text": "The most commonly used hashing algorithm is:",
+                "answers": {
+                    "1": "MD5",
+                    "2": "SHA-256",
+                    "3": "SHA-512",
+                    "4": "SHA-224"
+                },
+                "correct": "SHA-256"
+            },
+            "400": {
+                "text": "The main characteristic of asymmetric keys is:",
+                "answers": {
+                    "1": "Different keys for decryption and encryption",
+                    "2": "Same keys for decryption and encryption",
+                    "3": "Different encryption algorithms",
+                    "4": "Same encryption algorithms"
+                },
+                "correct": "Different keys for decryption and encryption"
+            },
+            "500": {
+                "text": "Two things needed to reverse a hash are:",
+                "answers": {
+                    "1": "Key, length of message",
+                    "2": "Key & hashing algorithm",
+                    "3": "Hashing algorithm & message length",
+                    "4": "Key and original message"
+                },
+                "correct": "Key, length of message"
+            }
+        }
+    },
+    "2": {
+        "name": "",
+        "questions": {
+            "100": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
                 "correct": ""
             },
             "200": {
                 "text": "",
-                "answer": {
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "300": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "400": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "500": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            }
+        }
+    },
+    "3": {
+        "name": "",
+        "questions": {
+            "100": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "200": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "300": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "400": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "500": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            }
+        }
+    },
+    "4": {
+        "name": "",
+        "questions": {
+            "100": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "200": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "300": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "400": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "500": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            }
+        }
+    },
+    "5": {
+        "name": "",
+        "questions": {
+            "100": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "200": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "300": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "400": {
+                "text": "",
+                "answers": {
+                    "1": "",
+                    "2": "",
+                    "3": "",
+                    "4": ""
+                },
+                "correct": ""
+            },
+            "500": {
+                "text": "",
+                "answers": {
                     "1": "",
                     "2": "",
                     "3": "",
@@ -25,3 +275,5 @@ export default {
         }
     }
 }
+
+export default data;
