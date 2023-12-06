2c7b740c37abc81246b79e0c4ebb8d1919208a1d
Stepan Susorov <steven.susorov@gmail.com>
2023-12-06 16:19:30 -0700

    Added functionality to the popup card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Player from "../util/Player";
 
 export default function Card(props: any): JSX.Element {
+    const [correct, setCorrect] = useState<number>(Number.MAX_VALUE);
+    const [answered, setAnswered] = useState<boolean>(false);
+
     function setNextPlayer() {
         let index : number = 0;
         for (let i=0; i<props.players.length; i++) {
@@ -14,27 +18,33 @@ export default function Card(props: any): JSX.Element {
         props.setPlayer(new_active_index, props.players[new_active_index].name, true);
     }
 
-    function handleAnswerClick(index: number) {
+    async function handleAnswerClick(index: number) {
         if (props.cardInfo.answerList[index] === props.cardInfo.answer) {
             props.players.filter((player: Player) => player.active).forEach((player: Player) => player.score += props.cardInfo.score);
             setNextPlayer();
+            setCorrect(index);
+            setAnswered(true);
         }
     }
 
     return (props.trigger) ? (
         <div className="flex items-center justify-center absolute top-0 left-0 z-40 w-full h-full rounded-lg bg-slate-200">
-            <button className="absolute right-0 top-0 p-3" onClick={() => props.setTrigger(false)}>
+            <button className="absolute right-0 top-0 p-3" onClick={() => {
+                props.setTrigger(false);
+                setCorrect(Number.MAX_VALUE);
+                setAnswered(false);
+            }}>
                 <img className="w-6 h-6 opacity-50 hover:opacity-100" src="close.png"/>
             </button>
             <div className="relative flex flex-col gap-2 items-center m-6">
                 <h1>{props.cardInfo.category}</h1>
                 <h1 className="break-words">{props.cardInfo.question}</h1>
-                <button className="border border-purple-80 rounded-md transition hover:bg-purple-80 hover:text-white w-[75%] py-1" onClick={() => {
+                <button className={`border border-purple-80 rounded-md transition w-[75%] py-1 ${correct === 1 ? "bg-emerald-200 hover:bg-emerald-300" : "hover:bg-purple-80 hover:text-white"} ${answered ? "pointer-events-none" : ""}`} onClick={() => {
                     handleAnswerClick(1);
                 }}>{props.cardInfo.answerList[1]}</button>
-                <button className="border border-purple-80 rounded-md transition hover:bg-purple-80 hover:text-white w-[75%] py-1">{props.cardInfo.answerList[2]}</button>
-                <button className="border border-purple-80 rounded-md transition hover:bg-purple-80 hover:text-white w-[75%] py-1">{props.cardInfo.answerList[3]}</button>
-                <button className="border border-purple-80 rounded-md transition hover:bg-purple-80 hover:text-white w-[75%] py-1">{props.cardInfo.answerList[4]}</button>
+                <button className={`border border-purple-80 rounded-md transition w-[75%] py-1 ${correct === 2 ? "bg-emerald-200 hover:bg-emerald-300" : "hover:bg-purple-80 hover:text-white"} ${answered ? "pointer-events-none" : ""}`}>{props.cardInfo.answerList[2]}</button>
+                <button className={`border border-purple-80 rounded-md transition w-[75%] py-1 ${correct === 3 ? "bg-emerald-200 hover:bg-emerald-300" : "hover:bg-purple-80 hover:text-white"} ${answered ? "pointer-events-none" : ""}`}>{props.cardInfo.answerList[3]}</button>
+                <button className={`border border-purple-80 rounded-md transition w-[75%] py-1 ${correct === 4 ? "bg-emerald-200 hover:bg-emerald-300" : "hover:bg-purple-80 hover:text-white"} ${answered ? "pointer-events-none" : ""}`}>{props.cardInfo.answerList[4]}</button>
             </div>
         </div>
     ) : (
