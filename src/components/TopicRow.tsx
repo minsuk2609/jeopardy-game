67cc771f3d7491acbeeed9f9c1c1488346437f72
Stepan Susorov <steven.susorov@gmail.com>
2023-12-06 18:32:41 -0700

    Added responsive spacing between elements

--- src/components/TopicRow.tsx
+++ src/components/TopicRow.tsx
@@ -2,12 +2,12 @@ import data from "../util/questions";
 
 export default function TopicColumn(props: any): JSX.Element {
     return (
-        <div className="flex gap-2">
-            <div className="text-sm sm:text-base w-12 h-20 xsm:h-20 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0">{data[1].name}</div>
-            <div className="text-sm sm:text-base w-12 h-20 xsm:h-20 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0">Topic 2</div>
-            <div className="text-sm sm:text-base w-12 h-20 xsm:h-20 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0">Topic 3</div>
-            <div className="text-sm sm:text-base w-12 h-20 xsm:h-20 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0">Topic 4</div>
-            <div className="text-sm sm:text-base w-12 h-20 xsm:h-20 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0">Topic 5</div>
+        <div className="flex gap-2 sm:gap-4">
+            <div className="text-sm sm:text-base w-12 h-24 xsm:h-24 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0 text-center">{data[1].name}</div>
+            <div className="text-sm sm:text-base w-12 h-24 xsm:h-24 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0 text-center">{data[2].name}</div>
+            <div className="text-sm sm:text-base w-12 h-24 xsm:h-24 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0 text-center">{data[3].name}</div>
+            <div className="text-sm sm:text-base w-12 h-24 xsm:h-24 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0 text-center">{data[4].name}</div>
+            <div className="text-sm sm:text-base w-12 h-24 xsm:h-24 xsm:w-16 sm:w-20 sm:h-20 flex justify-center items-center rotate-90 py-2 sm:rotate-0 text-center">{data[5].name}</div>
         </div>
     );
 }
